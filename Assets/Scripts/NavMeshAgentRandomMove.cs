7ebe4c211150129b02c7f669fc1f1f93e1d4b308
André McGrail <andrem@unity3d.com>
2019-02-08 15:56:57 +0100

    Added basic navagent random movement

---
+++ Assets/Scripts/NavMeshAgentRandomMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshAgentRandomMove : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public float wanderRange = 10f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        if (!agent)
+            enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!agent.hasPath)
+        {
+            var target = Random.insideUnitCircle * wanderRange;
+            NavMeshHit hit;
+            NavMesh.FindClosestEdge(new Vector3(target.x, 0f, target.y), out hit, NavMesh.AllAreas);
+            agent.destination = hit.position;
+        }
+    }
+}
