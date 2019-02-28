851e583c2346b880b754418a60bace0ba062af12
André McGrail <andrem@unity3d.com>
2019-02-28 17:07:51 +0100

    Added resolution limiter & removed low quality settings

---
+++ Assets/Scripts/Runtime/AppSettings.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.LWRP;
+
+[ExecuteAlways]
+public class AppSettings : MonoBehaviour
+{
+    public enum RenderRes
+    {
+        _Native,
+        _2440p,
+        _1080p,
+        _720p
+    }
+
+    public RenderRes maxRenderSize = RenderRes._720p;
+
+    void OnEnable()
+    {
+        Application.targetFrameRate = 60;
+
+        float res;
+        
+        switch (maxRenderSize)
+        {
+            case RenderRes._720p:
+                res = 720f;
+                break;
+            case RenderRes._1080p:
+                res = 1080f;
+                break;
+            case RenderRes._2440p:
+                res = 2440f;
+                break;
+            default:
+                res = Camera.main.pixelHeight;
+                break;
+        }
+        var renderScale = Mathf.Clamp(res / Camera.main.pixelHeight, 0.1f, 1.0f);
+        
+        LightweightRenderPipeline.asset.renderScale = renderScale;
+    }
+}
