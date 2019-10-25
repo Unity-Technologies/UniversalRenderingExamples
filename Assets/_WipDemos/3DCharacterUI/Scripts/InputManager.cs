c5a20c844222f0681bd365bc11093ebf275390ca
André McGrail <andrem@unity3d.com>
2019-10-25 16:25:20 +0200

    More work on 3D UI setup

--- Assets/_WipDemos/3DCharacterUI/Scripts/InputManager.cs
+++ Assets/_WipDemos/3DCharacterUI/Scripts/InputManager.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.EventSystems;
 
 public class InputManager : MonoBehaviour
 {
     public Camera mainCamera;
-
+    public NavMeshAgent agent;
+    public Animator animator;
+    
     private bool firstTouchHappened = false;
     private Vector3 touchStartPosition;
     private float groundZ = 0;
@@ -13,29 +16,34 @@ public class InputManager : MonoBehaviour
     
     void Update()
     {
-
         #if UNITY_EDITOR
 
         ProcessDesktopInputs();
 
-        
         #elif UNITY_IOS || UNITY_ANDROID
         
         ProcessMobileInputs();
 
         #endif
-        
 
+        if (agent.velocity.sqrMagnitude > 0.01f)
+        {
+            animator.SetBool("isMoving", true);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
     }
     
 
     void ProcessDesktopInputs()
     {
-
         //Left Click - Selection
-         if(Input.GetKeyDown(KeyCode.Mouse0)){
-
-            if (!EventSystem.current.IsPointerOverGameObject ()){
+         if(Input.GetKeyDown(KeyCode.Mouse0))
+         {
+            if (!EventSystem.current || !EventSystem.current.IsPointerOverGameObject ())
+            {
                 RaycastAgainstTheWorld(Input.mousePosition);
             }
          }
@@ -46,11 +54,8 @@ public class InputManager : MonoBehaviour
 
     void ProcessMobileInputs()
     {
-
-       
         if(Input.touchCount > 0)
         {
-            
             Touch firstTouch = Input.GetTouch(0);
 
              if (EventSystem.current.IsPointerOverGameObject(0))    // is the touch on the GUI
@@ -63,23 +68,18 @@ public class InputManager : MonoBehaviour
                     break; 
 
             }
-              
-            
         }
-        
     }
     
     void RaycastAgainstTheWorld(Vector3 inputPosition)
     {
-
         var ray = mainCamera.ScreenPointToRay(inputPosition);
         RaycastHit hit;
 
         if(Physics.Raycast(ray, out hit))
         {
+            agent.SetDestination(hit.point);
             //GameManager.Instance.ObjectClicked(hit.collider.gameObject, hit.point);
         }
-
     }
-    
 }
