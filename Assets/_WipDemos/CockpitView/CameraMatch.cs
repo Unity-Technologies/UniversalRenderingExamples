e8a543525b09d8fed7903595647797d14a8217c1
André McGrail <andrem@unity3d.com>
2020-01-31 16:42:34 +0100

    update

---
+++ Assets/_WipDemos/CockpitView/CameraMatch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class CameraMatch : MonoBehaviour
+{
+    public Camera camera;
+    private Camera m_ThisCamera;
+    private UniversalAdditionalCameraData m_CamData;
+    private UniversalAdditionalCameraData m_ThisCamData;
+
+    private void OnEnable()
+    {
+        if (!camera) return;
+
+        camera.TryGetComponent(out m_CamData);
+        TryGetComponent(out m_ThisCamera);
+        TryGetComponent(out m_ThisCamData);
+    }
+
+    private void Update()
+    {
+        throw new NotImplementedException();
+    }
+}
