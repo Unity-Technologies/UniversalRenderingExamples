733c86b56e633372c0763cf79269d00c0da01216
André McGrail <andrem@unity3d.com>
2020-02-04 15:05:44 +0100

    Updated 3D skybox with reversed camera setup and using stencil

---
+++ Assets/_CompletedDemos/3DSkybox/Scripts/SkyboxRenderFeature.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class SkyboxRenderFeature : ScriptableRendererFeature
+{
+
+    DrawSkyboxPass m_ScriptablePass;
+
+    public override void Create()
+    {
+        m_ScriptablePass = new DrawSkyboxPass(RenderPassEvent.AfterRenderingSkybox);
+
+        // Configures where the render pass should be injected.
+        m_ScriptablePass.renderPassEvent = RenderPassEvent.AfterRenderingOpaques;
+    }
+
+    // Here you can inject one or multiple render passes in the renderer.
+    // This method is called when setting up the renderer once per-camera.
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        renderer.EnqueuePass(m_ScriptablePass);
+    }
+}
+
+
