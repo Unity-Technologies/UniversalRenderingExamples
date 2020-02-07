dda748ab9e4c156403698e5b92d049ccda7df07b
Jonas Mortensen <jonas.mortensen@unity3d.com>
2020-02-07 10:55:45 +0100

    Renames and small changes

--- Assets/_CompletedDemos/3DSkybox/Scripts/SkyboxRenderFeature.cs
+++ Assets/_CompletedDemos/3DSkybox/Scripts/SkyboxRenderFeature.cs
@@ -5,21 +5,21 @@ using UnityEngine.Rendering.Universal;
 public class SkyboxRenderFeature : ScriptableRendererFeature
 {
 
-    DrawSkyboxPass m_ScriptablePass;
+    DrawSkyboxPass m_SkyboxPass;
 
     public override void Create()
     {
-        m_ScriptablePass = new DrawSkyboxPass(RenderPassEvent.AfterRenderingSkybox);
+        m_SkyboxPass = new DrawSkyboxPass(RenderPassEvent.AfterRenderingSkybox);
 
         // Configures where the render pass should be injected.
-        m_ScriptablePass.renderPassEvent = RenderPassEvent.AfterRenderingOpaques;
+        m_SkyboxPass.renderPassEvent = RenderPassEvent.AfterRenderingOpaques;
     }
 
     // Here you can inject one or multiple render passes in the renderer.
     // This method is called when setting up the renderer once per-camera.
     public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
     {
-        renderer.EnqueuePass(m_ScriptablePass);
+        renderer.EnqueuePass(m_SkyboxPass);
     }
 }
 
