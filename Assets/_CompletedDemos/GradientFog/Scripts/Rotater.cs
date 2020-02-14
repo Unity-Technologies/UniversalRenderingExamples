ec224ddeceb2fbefaa385d7a2e0fd3954b5d5279
Jonas Mortensen <jonasmortensen3@gmail.com>
2020-02-14 14:23:42 +0100

    Final changes

---
+++ Assets/_CompletedDemos/GradientFog/Scripts/Rotater.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotater : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, speed * Time.deltaTime);
+    }
+}
